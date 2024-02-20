9f1cbdf191687e928ba0d1156f2221eaae8bf10d
rayhankabir-me <devrayhankabir@gmail.com>
2024-02-21 02:16:27 +0600

    users controller and services - showing users

---
+++ src/users/users.controller.ts
@@ -0,0 +1,32 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  create(@Body() user: object) {
+    return this.usersService.create(user);
+  }
+
+  @Get()
+  findAll() {
+    return this.usersService.findAll();
+  }
+
+  // @Get(':id')
+  // findOne(@Param('id') id: string) {
+  //   return this.usersService.findOne(+id);
+  // }
+
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+  //   return this.usersService.update(+id, updateUserDto);
+  // }
+
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.usersService.remove(+id);
+  // }
+}
---
+++ src/users/users.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class UsersService {
+  private userLists = [
+    {
+      name: 'Rayhan Kabir',
+      id: 2,
+      role: 'Admin',
+    },
+    {
+      name: 'Ashikur Rahman',
+      id: 3,
+      role: 'Instructor',
+    },
+  ];
+  create(user) {
+    this.userLists.push(user);
+    return this.userLists;
+  }
+
+  findAll() {
+    return this.userLists;
+  }
+
+  // findOne(id: number) {
+  //   return `This action returns a #${id} user`;
+  // }
+
+  // update(id: number, updateUserDto: UpdateUserDto) {
+  //   return `This action updates a #${id} user`;
+  // }
+
+  // remove(id: number) {
+  //   return `This action removes a #${id} user`;
+  // }
+}
